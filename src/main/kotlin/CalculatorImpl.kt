0f714e012ba37d92b5713444a9c89b3bed7aa5ad
Israel Mendoza <xander_irsl@hotmail.com>
2024-06-12 23:28:06 -0600

    Added addition loop

---
+++ src/main/kotlin/CalculatorImpl.kt
@@ -0,0 +1,7 @@
+package dev.artisra
+
+class CalculatorImpl : Calculator {
+    override fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
 package dev.artisra
 
 fun main() {
-    val numbers = readln().split(" ").map { it.toInt() }
-    println(numbers.first() + numbers.last())
+    val calculator = CalculatorImpl()
+    ongoingCalculationPrompt(calculator, "/exit")
+    println("Bye!")
+}
+
+fun ongoingCalculationPrompt(calculator: Calculator, safeWord: String) {
+    while (true) {
+        val input = readlnOrNull()
+        if (input.equals(safeWord, ignoreCase = true)) break
+        if (input.isNullOrEmpty()) continue
+        val numbers = input.split(" ").map { it.toInt() }
+        val result = if (numbers.size > 1) {
+            calculator.add(numbers.first(), numbers.last())
+        } else {
+            calculator.add(numbers.first(), 0)
+        }
+        println(result)
+    }
 }
